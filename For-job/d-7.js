b8055f06f96dd50de82ff2413025ef7d6e0feda4
George <rahulsapnaji@gmail.com>
2025-04-30 17:39:43 +0530

    changes

--- For-job/d-7.js
+++ For-job/d-7.js
@@ -6,3 +6,17 @@ function Change(data, replace, newreplace) {
   console.log(dc1);
 }
 //Change(a1, "it", "is");
+
+function foo1(name) {
+  this.name = name;
+}
+
+foo1.prototype.greet = function () {
+  return `my name is ${this.name}`;
+};
+const a2 = new foo1("George");
+//console.log(a2.greet());
+Array.prototype.sum = function () {
+  return this.reduce((a, b) => a + b);
+};
+//console.log([1, 2, 3, 4, 5].sum());
